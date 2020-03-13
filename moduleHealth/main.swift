75dbf039a5fbf4d38d96a768fe03511e1144ba20
Bram Huenaerts <bram@chronux.be>
2020-03-13 22:20:06 +0100

    initial project setup

---
+++ moduleHealth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  moduleHealth
+//
+//  Created by Bram Huenaerts on 13/03/2020.
+//  Copyright © 2020 chronux bv. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
